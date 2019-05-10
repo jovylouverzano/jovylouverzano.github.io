8d4515366fd1a65eb569ba64538a6a3768c866ea
jovylouverzano <jovylouverzano@gmail.com>
2019-05-10 23:13:53 +0800

    Uploading Website

---
+++ js/main.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+
+    //navbar background change from transparent to solid
+    var $document = $(document),
+        $element = $('.navbar'),
+        navbarDefault = 'navbar-default';
+    navbarTransparent = 'navbar-transparent';
+    fadeInDown = 'fadeInDown';
+
+
+    $document.scroll(function () {
+        if ($document.scrollTop() >= 100) {
+            $element.addClass(navbarDefault);
+            $element.removeClass(navbarTransparent);
+            $element.addClass(fadeInDown);
+        } else {
+            $element.addClass(navbarTransparent);
+            $element.removeClass(navbarDefault);
+            $element.removeClass(fadeInDown);
+        }
+    })
+
+    //navbar background change from transparent to solid end
+
+    
+    //smooth-scrolling
+    var scrollLink = $('.scroll');
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000)
+    })
+
+    //smooth-scrolling end 
+
+
+
+    //highlight nav-links
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+        scrollLink.each(function () {
+            var sectionOffset = $(this.hash).offset().top - 50;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        })
+    })
+
+    //highlight nav-links end
+
+
+    // skillbar animation
+    var offsetTop = $('.skill-area').offset().top;
+
+    $(window).scroll(function () {
+        var height = $(window).height();
+        if ($(window).scrollTop() + height >= offsetTop) {
+            jQuery('.skillbar').each(function () {
+                jQuery(this).find('.skillbar-bar').animate({
+                    width: jQuery(this).attr('data-percent')
+                }, 3000);
+            });
+        }
+    });
+
+    // skillbar end
+
+    
+
+    // for WOW.js library ie: fadeInUp
+    
+    new WOW().init();
+
+    // WOW.js library end
+    
+    
+    // for baguetteBox.js library ie: certificates section
+    
+    baguetteBox.run('.certificates-gallery');
+
+    // WOW.js library end
+    
+
+});
